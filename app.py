248059085ee59df10275dd2e2e710f9410f8f237
JavierOramas <javiale2000@gmail.com>
2021-03-03 12:51:38 -0500

    -

--- app.py
+++ app.py
@@ -1,23 +1,74 @@
-from flask import Flask, request, send_from_directory
-import qrcode
-import PIL.Image as img
-import gunicorn
+# from flask import Flask, request, send_from_directory
+# import qrcode
+# import PIL.Image as img
+# import gunicorn
 
-app = Flask(__name__)
-@app.route('/')
-def home():
-    return '<h1> Hello There! </h1>'
-@app.route('/<data>')
-def generate_qr(data):
-    qr = qrcode.QRCode(
-        version=1,
-        box_size=10,
-        border=2)
-    qr.add_data(data)
-    qr.make(fit=True)
-    img = qr.make_image(fill='black', back_color='white')
-    img.save('image/qrcode.png')
-    return send_from_directory(directory='./image', filename='qrcode.png', as_attachment=True)
+# app = Flask(__name__)
+# @app.route('/')
+# def home():
+#     return '<h1> Hello There! </h1>'
+# @app.route('/<data>')
+# def generate_qr(data):
+#     qr = qrcode.QRCode(
+#         version=1,
+#         box_size=10,
+#         border=2)
+#     qr.add_data(data)
+#     qr.make(fit=True)
+#     img = qr.make_image(fill='black', back_color='white')
+#     img.save('image/qrcode.png')
+#     return send_from_directory(directory='./image', filename='qrcode.png', as_attachment=True)
     
-if __name__ == "__main__":
-    app.run()
+# if __name__ == "__main__":
+#     app.run()
+
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+
+@app.route('/getmsg/', methods=['GET'])
+def respond():
+    # Retrieve the name from url parameter
+    name = request.args.get("name", None)
+
+    # For debugging
+    print(f"got name {name}")
+
+    response = {}
+
+    # Check if user sent a name at all
+    if not name:
+        response["ERROR"] = "no name found, please send a name."
+    # Check if the user entered a number not a name
+    elif str(name).isdigit():
+        response["ERROR"] = "name can't be numeric."
+    # Now the user entered a valid name
+    else:
+        response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
+
+    # Return the response in json format
+    return jsonify(response)
+
+@app.route('/post/', methods=['POST'])
+def post_something():
+    param = request.form.get('name')
+    print(param)
+    # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
+    if param:
+        return jsonify({
+            "Message": f"Welcome {name} to our awesome platform!!",
+            # Add this option to distinct the POST request
+            "METHOD" : "POST"
+        })
+    else:
+        return jsonify({
+            "ERROR": "no name found, please send a name."
+        })
+
+# A welcome message to test our server
+@app.route('/')
+def index():
+    return "<h1>Welcome to our server !!</h1>"
+
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
