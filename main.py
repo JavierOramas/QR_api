0b6e5fcc14bcb7729982eb71ce99d9f4e63f7165
JavierOramas <javiale2000@gmail.com>
2021-03-03 12:24:42 -0500

    -

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def generate_qr(data):
     qr.make(fit=True)
     img = qr.make_image(fill='black', back_color='white')
     img.save('image/qrcode.png')
-    return send_from_directory(directory='./image', filename='qrcode.png', as_attachment=True)
-
+    send_from_directory(directory='./image', filename='qrcode.png', as_attachment=True)
+    return 'ok'
 if __name__ == "__main__":
     app.run()
