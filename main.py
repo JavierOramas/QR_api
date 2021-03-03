fe6063cf18f514ccd3a2e79f47229d7f66e7b424
JavierOramas <javiale2000@gmail.com>
2021-03-03 12:33:49 -0500

    -

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from flask import Flask, request, send_from_directory
 import qrcode
 import PIL.Image as img
 app = Flask(__name__)
-
+@app.route('/')
+def home():
+    return '<h1> Hello There! </h1>'
 @app.route('/<data>')
 def generate_qr(data):
     qr = qrcode.QRCode(
